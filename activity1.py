93c9bdc05757c091418d755cdaec86765ca3a57c
nuh-dot <singhmaurispajan@gmail.com>
2024-09-22 12:23:29 +0800

    activity1.py

---
+++ activity1.py
@@ -0,0 +1,13 @@
+Type "help", "copyright", "credits" or "license" for more information.
+>>> num = 6 + 3
+>>> print(num)
+9
+>>> num = 6 - 3
+>>> print(num)
+3
+>>> num = 6 * 3
+>>> print(num)
+18
+>>> num = 6 / 3
+>>> print(num)
+2.0
