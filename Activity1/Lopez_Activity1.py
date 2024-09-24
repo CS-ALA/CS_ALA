70489c4a319e25c6f4c44bc8b6c5b026f51a09bd
RagenutXD <87089028+RagenutXD@users.noreply.github.com>
2024-09-24 19:54:19 +0800

    Add files via upload

---
+++ Activity1/Lopez_Activity1.py
@@ -0,0 +1,8 @@
+x = input("Enter first number: ")
+y= input("Enter second number: ")
+
+
+print("Addition: ",int(x) + int(y))
+print("Subtraction: ",int(x) - int(y))
+print("Multiplication: ",int(x) * int(y))
+print("Division: ", int(x)/int(y))
