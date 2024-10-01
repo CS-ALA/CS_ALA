a4db726ad6d2e1c99b4b031888ecaad45ad1be98
deanthebean <mayettecantila@gmail.com>
2024-10-01 11:07:50 +0800

    Add files via upload

---
+++ Activity1/Cantila_Activity1.py
@@ -0,0 +1,11 @@
+print("A program that takes two inputs, and adds, subtracts, multiplies, and divides them.")
+
+a, b = map(int,input("Enter the numbers (Separated by space): ").split())
+
+
+print("addition= ",a+b)
+print("Subtraction= ",a-b)
+print("Multiplication= ",a*b)
+print("Division= ",a/b)
+
+
